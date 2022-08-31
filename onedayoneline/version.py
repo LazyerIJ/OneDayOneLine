53cb123ea189b2ee4bd4958147ff4a5369312446
InJu Kim <inju.kim@seizedata.com>
2022-08-31 10:04:36 +0900

    docs: version update -> v0.0.0

---
+++ onedayoneline/version.py
@@ -0,0 +1,7 @@
+ODOL_MAJOR_VERSION = 0
+ODOL_MINOR_VERSION = 0
+ODOL_PATCH_VERSION = 0
+
+__version__ = "{}.{}.{}".format(ODOL_MAJOR_VERSION,
+                                ODOL_MINOR_VERSION,
+                                ODOL_PATCH_VERSION)
