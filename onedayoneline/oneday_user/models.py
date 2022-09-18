7c91b1ba70a9dd70d8e07ed520f8bb2dbf145a70
Areum <gwj01058@naver.com>
2022-09-18 23:57:37 +0900

    feat : django 예제 따라하기 (회원가입 기능추가)

--- onedayoneline/oneday_user/models.py
+++ onedayoneline/oneday_user/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Oneday_user(models.Model):
     username = models.CharField(max_length=64,
                                 verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128,
+                                  verbose_name='사용자이메일')
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True,
--- onedayoneline/oneday_user/views.py
+++ onedayoneline/oneday_user/views.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
+from .models import Oneday_user
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)     # username : register의 name 항목
+        useremail = request.POST.get('useremail', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
+
+        res_data = {}
+
+        if not (username and useremail and password and re_password):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        elif password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            oneday_user = Oneday_user(
+                username=username,
+                useremail=useremail,
+                password=make_password(password)
+            )
+
+            oneday_user.save()
+
+        return render(request, 'register.html', res_data)
